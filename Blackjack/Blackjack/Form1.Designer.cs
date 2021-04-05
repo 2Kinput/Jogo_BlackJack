a35442b493bfa9c88caabb040d18f47f96ceb5ee
2Kinput <62145628+2Kinput@users.noreply.github.com>
2021-04-05 10:58:51 -0300

    Add files via upload

---
+++ Blackjack/Blackjack/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Blackjack
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbl_Legendas = new System.Windows.Forms.Label();
+            this.lbl_Pontos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(77, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 254);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Jogar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reiniciar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbl_Legendas
+            // 
+            this.lbl_Legendas.AutoSize = true;
+            this.lbl_Legendas.Location = new System.Drawing.Point(28, 419);
+            this.lbl_Legendas.Name = "lbl_Legendas";
+            this.lbl_Legendas.Size = new System.Drawing.Size(47, 15);
+            this.lbl_Legendas.TabIndex = 3;
+            this.lbl_Legendas.Text = "Pontos:";
+            this.lbl_Legendas.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_Pontos
+            // 
+            this.lbl_Pontos.AutoSize = true;
+            this.lbl_Pontos.Location = new System.Drawing.Point(77, 419);
+            this.lbl_Pontos.Name = "lbl_Pontos";
+            this.lbl_Pontos.Size = new System.Drawing.Size(12, 15);
+            this.lbl_Pontos.TabIndex = 4;
+            this.lbl_Pontos.Text = "_";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 446);
+            this.Controls.Add(this.lbl_Pontos);
+            this.Controls.Add(this.lbl_Legendas);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BlackJack (Versão 1.0)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbl_Legendas;
+        private System.Windows.Forms.Label lbl_Pontos;
+    }
+}
+
---
+++ Blackjack/Blackjack/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blackjack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        int pontos = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x;
+            Random sorteio = new Random();
+            x = sorteio.Next(1, 14);
+
+            switch (x)
+            {
+                case 1:
+                     pictureBox1.Image = Properties.Resources.a;
+                    pontos += 1;
+                    break;
+                 case 2:
+                    pictureBox1.Image = Properties.Resources._2;
+                    pontos += 2;
+                    break;
+                case 3:
+                     pictureBox1.Image = Properties.Resources._3;
+                    pontos += 3;
+                    break;
+                case 4:
+                     pictureBox1.Image = Properties.Resources._4;
+                    pontos += 4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources._5;
+                    pontos += 5;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources._6;
+                    pontos += 6;
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources._7;
+                    pontos += 7;
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources._8;
+                    pontos += 8;
+                    break;
+                case 9:
+                    pictureBox1.Image = Properties.Resources._9;
+                    pontos += 9;
+                    break;
+                case 10:
+                    pictureBox1.Image = Properties.Resources._10;
+                    pontos += 10;
+                    break;
+                case 11:
+                    pictureBox1.Image = Properties.Resources.J;
+                    pontos += 11;
+                    break;
+                case 12:
+                    pictureBox1.Image = Properties.Resources.Q;
+                    pontos += 12;
+                    break;
+                case 13:
+                    pictureBox1.Image = Properties.Resources.K;
+                    pontos += 13;
+                    break;
+
+            }
+           
+            if (pontos <= 21)
+            {
+                lbl_Pontos.Text = Convert.ToString(pontos);
+            }
+            else
+            {
+                button2.Enabled = true;
+                button1.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Reiniciar
+            pontos = 0;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            lbl_Pontos.Text = "0";
+            pictureBox1.Image = Properties.Resources._0;
+        }
+    }
+}
